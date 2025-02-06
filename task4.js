30b1872202fb60fad6d6598686f62a3c05604284
sedragalal <sedragalal850@gmail.com>
2025-02-07 01:27:15 +0200

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn');
+const p = document.getElementById('quote');
+button.addEventListener('click', () => {
+    p.innerText = 'success is not final failure is not fatal it is the courage to continue that counts';
+    });
